ec2b7cc51a55ca6acfb61f6360ece1aebd319fd6
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-06-20 17:33:21 -0300

    Default Week state

--- components/Week/Week.tsx
+++ components/Week/Week.tsx
@@ -6,9 +6,10 @@ import { getUserDataByEmail } from '@/app/utils/user';
 import DayCard from '../DayCard/DayCard';
 import { Day, List, TWeek } from '@/types/Types';
 import { getListByID, getListsByEmail } from '@/app/utils/list';
+import LoaderWithText from '../LoaderWithText/LoaderWithText';
 
 export const Week = () => {
-  const [days, setDays] = useState<Day[]>([]);
+  const [days, setDays] = useState<Day[]>();
   const [lists, setLists] = useState<List[]>([]);
 
   const session = useSession();
@@ -33,7 +34,19 @@ export const Week = () => {
       }
       // setIsLoading(false);
     };
-    if (session.status === 'authenticated') getUserDataAndLists();
+    if (session.status === 'authenticated') {
+      getUserDataAndLists();
+    } else {
+      setDays([
+        { name: 'Monday', number: 1, listId: '', date: '' },
+        { name: 'Tuesday', number: 2, listId: '', date: '' },
+        { name: 'Wednesday', number: 3, listId: '', date: '' },
+        { name: 'Thursday', number: 4, listId: '', date: '' },
+        { name: 'Friday', number: 5, listId: '', date: '' },
+        { name: 'Saturday', number: 6, listId: '', date: '' },
+        { name: 'Sunday', number: 7, listId: '', date: '' },
+      ]);
+    }
   }, [session.data?.user?.email, session.status]);
 
   useEffect(() => {
@@ -49,7 +62,7 @@ export const Week = () => {
 
   return (
     <>
-      {days &&
+      {days ? (
         days.map((day) => (
           <DayCard
             key={day.name}
@@ -57,8 +70,12 @@ export const Week = () => {
             lists={lists}
             name={day.name}
             date={day.date}
+            number={day.number}
           />
-        ))}
+        ))
+      ) : (
+        <LoaderWithText text="Fetching week data" />
+      )}
     </>
   );
 };
